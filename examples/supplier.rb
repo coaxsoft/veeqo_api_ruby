521258b6816eb7820bbb1decebf0331707957cdc
Yurkiv Misha <jurkiw.misha@gmail.com>
2016-11-29 17:46:02 +0200

    fixed supplier example

--- examples/supplier.rb
+++ examples/supplier.rb
@@ -10,21 +10,21 @@ puts @suppliers
 
 # Create supplier
 supplier_data = {
-  'name': 'test_supplier',
-  'address_line_1': 'test',
-  'address_line_2': 'test',
-  'city': 'test',
-  'region': 'test',
-  'country': 'test',
-  'post_code': 'test',
-  'sales_contact_name': 'test',
-  'sales_contact_email': 'test'
+  name: 'test_supplier',
+  address_line_1: 'test',
+  address_line_2: 'test',
+  city: 'test',
+  region: 'test',
+  country: 'test',
+  post_code: 'test',
+  sales_contact_name: 'test',
+  sales_contact_email: 'test'
 }
 puts Veeqo::Supplier.create(supplier_data)
 
 @supplier = @suppliers[0]
 supplier_data = {
-  'name': 'test_supplier_1'
+  name: 'test_supplier_1'
 }
 puts Veeqo::Supplier.update(@supplier, supplier_data)
 
