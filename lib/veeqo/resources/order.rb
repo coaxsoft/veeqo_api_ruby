6820ed226376be259693e6d70a5766043ef2fffe
Andriy Byalyk <rewrite.andriy@gmail.com>
2017-10-19 10:58:05 +0300

    Transform or use array type for params

--- lib/veeqo/resources/order.rb
+++ lib/veeqo/resources/order.rb
@@ -50,7 +50,7 @@ module Veeqo
 
     def self.bulk_tagging(resource_ids, tag_ids, params = {})
       raise ArgumentError if resource_ids.empty? || tag_ids.empty?
-      post 'https://app.veeqo.com/bulk_tagging', { order_ids: resource_ids, tag_ids: tag_ids }.merge(params)
+      post 'https://app.veeqo.com/bulk_tagging', { order_ids: [*resource_ids], tag_ids: [*tag_ids] }.merge(params)
     end
   end
 end
